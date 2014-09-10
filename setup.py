21d518849d3f1f0da51446f2007c063e06fa190c
Shichao An <shichao.an@nyu.edu>
2014-09-09 23:45:26 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from twphotos import __version__
+
+
+setup(
+    name='115wangpan',
+    version=__version__,
+    description="Unofficial Python API wrapper for 115.com",
+    long_description=open('README.rst').read(),
+    keywords='115 wangpan pan cloud lixian',
+    author='Shichao An',
+    author_email='shichao.an@nyu.edu',
+    url='https://github.com/shichao-an/115wangpan',
+    license='BSD',
+    install_requires=open('requirements.txt').read().splitlines(),
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+)
