7979118d9b62635b630e37b7db70cf0c1ca70f4e
Shichao An <shichao.an@nyu.edu>
2014-09-22 23:59:58 -0700

    v0.2.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='115wangpan',
-    version='0.2.2',
+    version='0.2.3',
     description="Unofficial Python API wrapper for 115.com",
     long_description=open('README.rst').read(),
     keywords='115 wangpan pan cloud lixian',
