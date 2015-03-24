829c4baa885121e57271d7b2f32d50e9d76e9305
Shichao An <shichao.an@nyu.edu>
2015-03-24 00:20:42 -0700

    Rewrote tasks-related tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-import time
+import datetime
 import os
+import time
 from unittest import TestCase
 from u115.api import API, Torrent, Directory, File, TaskError
 from u115.utils import pjoin
@@ -21,11 +22,176 @@ TEST_TORRENT2 = {
     'info_hash': 'd1fc55cc7547881884d01c56ffedd92d39d48847',
 }
 
-TEST_TARGET_URL = {
+TEST_TARGET_URL1 = {
     'url': 'http://download.thinkbroadband.com/1MB.zip',
     'info_hash': '5468537b4e05bd8f69bfe17e59e7ad85115',
 }
+
+TEST_TARGET_URL2 = {
+    'url': open(pjoin(DATA_DIR, 'magnet.txt')).read().strip(),
+    'info_hash': '1f9f293e5e4ef63eba76d3485fe54577737df0e8',
+}
 TEST_COOKIE_FILE = pjoin(DATA_DIR, '115cookies')
+
+
+def is_task_created(tasks, info_hash):
+    """
+    Check if a task is created against a hash value
+
+    :param list tasks: a list of :class:`API.Task` objects
+    :param str info_hash: hash value of the target task
+
+    """
+    for task in tasks:
+        if task.info_hash == info_hash:
+            return True
+    else:
+        return False
+
+
+class TaskTests(TestCase):
+    """Test general task interface"""
+    def setUp(self):
+        # Initialize a new API instance
+        self.api = API()
+        self.api.login(section='test')
+
+    def test_get_tasks(self):
+        filename1 = TEST_TORRENT1['filename']
+        filename2 = TEST_TORRENT2['filename']
+        url1 = TEST_TARGET_URL1['url']
+        url2 = TEST_TARGET_URL2['url']
+        # Test creation
+        assert self.api.add_task_bt(filename1)
+        assert self.api.add_task_bt(filename2)
+        assert self.api.add_task_url(url1)
+        assert self.api.add_task_url(url2)
+        # Test count
+        tasks = self.api.get_tasks()
+        assert len(tasks) == 4
+        tasks = self.api.get_tasks(3)
+        assert len(tasks) == 3
+
+    def test_task_attrs(self):
+        filename1 = TEST_TORRENT1['filename']
+        url1 = TEST_TARGET_URL1['url']
+        assert self.api.add_task_bt(filename1)
+        assert self.api.add_task_url(url1)
+        tasks = self.api.get_tasks()
+        for task in tasks:
+            assert isinstance(task.add_time, datetime.datetime)
+            assert isinstance(task.last_update, datetime.datetime)
+            assert isinstance(task.left_time, int)
+            assert task.name
+            assert task.move in [0, 1, 2]
+            assert task.peers >= 0
+            assert task.percent_done >= 0
+            assert task.size >= 0
+            assert task.size_human
+            assert isinstance(task.status, int)
+            assert task.status_human
+
+    def tearDown(self):
+        # Clean up all tasks
+        tasks = self.api.get_tasks()
+        for task in tasks:
+            task.delete()
+
+
+class BitTorrentTaskTests(TestCase):
+    """
+    Test torrent-based BitTorrent tasks
+    """
+    def setUp(self):
+        # Initialize a new API instance
+        self.api = API()
+        self.api.login(section='test')
+
+    def test_add_task_bt_select_false(self):
+        """
+        `API.add_task_bt(filename, select=False)`
+        """
+        filename = TEST_TORRENT1['filename']
+        info_hash = TEST_TORRENT1['info_hash']
+        assert self.api.add_task_bt(filename)
+        tasks = self.api.get_tasks()
+        assert is_task_created(tasks, info_hash)
+
+    def test_add_task_bt_select_true(self):
+        """
+        `API.add_task_bt(filename, select=True)`
+        """
+        filename = TEST_TORRENT2['filename']
+        info_hash = TEST_TORRENT2['info_hash']
+        torrent = self.api.add_task_bt(filename, select=True)
+        assert isinstance(torrent, Torrent)
+        torrent.files[0].unselect()
+        torrent.files[1].unselect()
+        assert len(torrent.selected_files) == torrent.file_count - 2
+        assert torrent.submit()
+        tasks = self.api.get_tasks()
+        assert is_task_created(tasks, info_hash)
+
+    def test_delete_tasks(self):
+        """
+        `Task.delete()`
+        """
+        filename = TEST_TORRENT2['filename']
+        info_hash = TEST_TORRENT2['info_hash']
+        assert self.api.add_task_bt(filename)
+        tasks = self.api.get_tasks()
+        is_task_created = False
+        for task in tasks:
+            if task.info_hash == info_hash:
+                is_task_created = True
+                assert not task.is_deleted
+                assert task.delete()
+                with self.assertRaises(TaskError) as cm:
+                    task.delete()
+                assert cm.exception.message == 'This task is already deleted.'
+                assert task.is_deleted
+                break
+        else:
+            assert is_task_created
+
+    def tearDown(self):
+        # Clean up all tasks
+        tasks = self.api.get_tasks()
+        for task in tasks:
+            task.delete()
+
+
+class URLTaskTests(TestCase):
+    def setUp(self):
+        # Initialize a new API instance
+        self.api = API()
+        self.api.login(section='test')
+
+    def test_add_task_url_http(self):
+        """
+        `API.add_task_url(target_url=HTTP)`
+        """
+        url = TEST_TARGET_URL1['url']
+        info_hash = TEST_TARGET_URL1['info_hash']
+        assert self.api.add_task_url(url)
+        tasks = self.api.get_tasks()
+        is_task_created(tasks, info_hash)
+
+    def test_add_task_url_magnet(self):
+        """
+        `API.add_task_url(target_url=MAGNET)`
+        """
+        url = TEST_TARGET_URL2['url']
+        info_hash = TEST_TARGET_URL2['info_hash']
+        assert self.api.add_task_url(url)
+        tasks = self.api.get_tasks()
+        is_task_created(tasks, info_hash)
+
+    def tearDown(self):
+        # Clean up all tasks
+        tasks = self.api.get_tasks()
+        for task in tasks:
+            task.delete()
 
 
 class TestAPI(TestCase):
@@ -101,32 +267,6 @@ class TestAPI(TestCase):
                 assert d2.delete()
                 d1.reload()
                 assert d1.count == d1_count - 1
-
-    def test_add_delete_task_bt(self):
-        h1 = TEST_TORRENT1['info_hash']
-        h2 = TEST_TORRENT2['info_hash']
-        tasks = self.api.get_tasks(LARGE_COUNT)
-        for task in tasks:
-            if task.info_hash == h1:
-                assert task.delete()
-            if task.info_hash == h2:
-                assert task.delete()
-        assert self.api.add_task_bt(TEST_TORRENT1['filename'])
-        u = self.api.add_task_bt(TEST_TORRENT2['filename'], select=True)
-        assert isinstance(u, Torrent)
-        files = u.files
-        file_count = u.file_count
-        files[0].unselect()
-        files[1].unselect()
-        assert len(u.selected_files) == file_count - 2
-        assert u.submit()
-
-    def test_add_task_url(self):
-        tasks = self.api.get_tasks()
-        for t in tasks:
-            if t.info_hash == TEST_TARGET_URL['info_hash']:
-                t.delete()
-        assert self.api.add_task_url(TEST_TARGET_URL['url'])
 
     def test_search(self):
         """Directory is assumed to have more than 40 torrent files"""
